eab3466b0e10fe312351f9d8c0b5cdd1b2398a25
Guenter Bloemer <guenterbloemer@gmail.com>
2017-03-23 20:42:47 -0300

    Adicionadas racas e classes

--- script_rpg/classes.sql
+++ script_rpg/classes.sql
@@ -1,6 +1,7 @@
 DELETE FROM classe;
 VACUUM;
 
+--codigo,nome,destreza,forca,agilidade,inteligencia,constituicao,espirito,vigor,pericia,hp_nivel,mp_nivel,classe_ant,des_primario,for_primario,agi_primario,int_primario,sabedoria,att_extra
 insert into classe values (1, 'Cavaleiro', 5, 5, 0, 0, 5, 5, 0, 5, 90, 35, null, 0, 0, 0, 0, 5, 0);
 insert into classe values (2, 'Ladrao', 0, 0, 10, 0, 0, 0, 0, 5, 65, 60, null, 0, 0, 0, 0, 10, 0);
 insert into classe values (3, 'Mago', 0, 0, 0, 10, 0, 5, 0, 5, 30, 95, null, 0, 0, 0, 0, 5, 0);
@@ -17,7 +18,7 @@ insert into classe values (11, 'Exterminador Mistico', 0, 0, 10, 0, 0, 0, 0, 10,
 insert into classe values (12, 'Lupino', 0, 15, 0, 0, 20, 0, 0, 15, 150, 100, null, 0, 0, 0, 0, 0, 0);
 insert into classe values (13, 'Lycan', 0, 25, 0, 0, 55, 0, 0, 20, 400, 100, 12, 0, 1, 0, 0, 0, 0);
 
-insert into classe values (14, 'Assassino', 0, 0, 20, 0, 10, 10, 0, 0, 175, 75, 2, 0, 0, 1, 0, 10, 0);
+insert into classe values (14, 'Assassino', 0, 0, 20, 0, 10, 10, 0, 0, 175, 75, 2, 0, 0, 0, 0, 10, 0);
 insert into classe values (15, 'Monge', 0, 10, 10, 0, 0, 20, 0, 10, 100, 150, 5, 0, 1, 0, 0, 0, 0);
 insert into classe values (16, 'Cavaleiro Negro', 0, 15, 0, 0, 15, 0, 0, 10, 200, 50, 1, 0, 1, 0, 0, 10, 0);
 insert into classe values (17, 'Bispo', 0, 0, 0, 10, 10, 20, 0, 0, 125, 125, 5, 0, 0, 0, 1, 10, 0);
@@ -39,7 +40,7 @@ insert into classe values (27, 'Combatente', 0, 50, 0, 0, 0, 0, 0, 0, 175, 75, 1
 insert into classe values (28, 'Mestre Arcano', 0, 0, 0, 30, 0, 20, 0, 20, 200, 550, 8, 0, 0, 0, 1, 20, 0);
 insert into classe values (29, 'Shogun', 0, 20, 20, 0, 20, 20, 0, 20, 415, 335, 10, 0, 0, 1, 0, 0, 0);
 insert into classe values (30, 'Guerreiro da Virtude', 0, 20, 0, 0, 20, 40, 0, 20, 250, 500, 15, 0, 1, 0, 0, 0, 0);
-insert into classe values (31, 'Arauto da Morte', 0, 0, 50, 0, 15, 10, 0, 0, 450, 300, 14, 0, 0, 1, 0, 25, 0);
+insert into classe values (31, 'Arauto da Morte', 0, 0, 50, 0, 15, 10, 0, 0, 450, 300, 14, 0, 0, 0, 0, 25, 0);
 insert into classe values (32, 'Cavaleiro da Morte', 0, 30, 0, 0, 20, 20, 0, 15, 600, 150, 16, 0, 1, 0, 0, 15, 0);
 
 insert into classe values (33, 'Mimico', 10, 10, 10, 10, 10, 10, 0, 10, 150, 150, 7, 0, 0, 0, 0, 10, 0);
@@ -59,7 +60,7 @@ insert into classe values (41, 'Alquimista', 0, 0, 0, 20, 15, 15, 0, 0, 175, 75,
 insert into classe values (43, 'Meio Celestial', 0, 10, 0, 10, 0, 20, 0, 0, 100, 200, null, 0, 0, 0, 0, 10, 0);
 insert into classe values (44, 'Celestial', 0, 20, 0, 20, 0, 40, 0, 0, 200, 400, 43, 0, 1, 0, 0, 20, 0);
 
-insert into classe values (45, 'Elementalista', 0, 0, 0, 0, 0, 0, 0, 30, 250, 50, 19, 0, 0, 0, 1, 30, 0);
+insert into classe values (45, 'Elementalista Vento', 0, 0, 0, 0, 0, 0, 0, 30, 250, 50, 19, 0, 0, 0, 1, 30, 0);
 
 insert into classe values (46, 'Paladino', 0, 20, 0, 0, 30, 30, 0, 0, 550, 200, 25, 0, 0, 0, 1, 20, 0);
 insert into classe values (47, 'Mestre Lanceiro', 0, 0, 30, 0, 0, 0, 0, 50, 450, 300, 21, 0, 0, 0, 1, 20, 0);
@@ -76,6 +77,21 @@ insert into classe values (55, 'Meio Dragao', 10, 10, 10, 10, 10, 10, 10, 10, 17
 insert into classe values (56, 'Herdeiro de Sangue (Trevas)', 20, 20, 20, 20, 20, 20, 20, 20, 350, 350, 55, 0, 0, 0, 0, 10, 20);
 
 insert into classe values (57, 'Ilusionaista', 0, 0, 10, 20, 0, 0, 0, 0, 100, 150, 2, 0, 0, 0, 0, 20, 0);
+
+insert into classe values (60, 'Elementalista de Fogo', 0, 0, 0, 0, 0, 0, 0, 30, 100, 200, 19, 0, 0, 0, 0, 30, 0);
+insert into classe values (61, 'Mestre do Fogo', 0, 0, 0, 0, 0, 0, 0, 60, 300, 550, 60, 0, 0, 0, 0, 60, 0);
+
+insert into classe values (65, 'Meio Abissal', 0, 10, 10, 0, 10, 10, 0, 0, 150, 150, null, 0, 0, 0, 0, 10, 0);
+insert into classe values (66, 'Abissal', 0, 20, 20, 0, 20, 20, 0, 0, 300, 300, 65, 0, 0, 0, 0, 20, 0);
+
+insert into classe values (70, 'Lacaio de Sangue', 0, 10, 0, 10, 0, 0, 0, 10, 90, 90, null, 0, 0, 0, 0, 10, 0);
+insert into classe values (71, 'Conde das Sombras', 0, 20, 0, 20, 0, 0, 0, 20, 175, 175, 70, 0, 0, 0, 0, 20, 0);
+insert into classe values (72, 'Imperador das Trevas', 0, 30, 0, 30, 0, 0, 0, 30, 350, 350, 71, 0, 0, 0, 0, 30, 0);
+
+insert into classe values (75, 'Trovador/Dançarino', 0, 0, 0, 0, 0, 0, 0, 0, 80, 170, 4, 0, 0, 0, 0, 50, 0);
+insert into classe values (76, 'Bardo/Encantador', 0, 0, 0, 0, 0, 0, 0, 0, 250, 500, 75, 0, 0, 0, 0, 100, 0);
+
+insert into classe values (79, 'Lorde celestial', 0, 30, 0, 30, 0, 60, 0, 0, 300, 700, 44, 0, 0, 0, 0, 30, 0);
 
 insert into classe values(500, 'Wyvern', 5, 40, 35, 5, 35, 20, 0, 20, 750, 200, null, 0, 0, 0, 0, 10, 0);
 insert into classe values(501, 'Dragao Platinado', 50, 80, 100, 50, 120, 120, 50, 80, 2500, 2000, null, 0, 0, 0, 0, 80, 0);
